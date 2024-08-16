7eff272b7d73d1d53bcd4c90d7eab80d78332163
willSchnere <schnerwill@gmail.com>
2024-08-16 09:46:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('whatsapp').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/5541997538494';
+});
+
+document.getElementById('instagram').addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com/'; 
+});
+
+document.getElementById('twitter').addEventListener('click', function() {
+    window.location.href = 'https://x.com/'; 
+});
+
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.location.href = 'https://br.linkedin.com/'; 
+});
